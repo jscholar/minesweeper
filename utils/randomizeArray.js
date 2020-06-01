c0022118be76e73c274d85333c40145c235e3654
Jin Cai <jinruncai@gmail.com>
2020-06-01 11:37:14 -0700

    Document randomizeArray

--- utils/randomizeArray.js
+++ utils/randomizeArray.js
@@ -1,4 +1,9 @@
+/**
+ * Shuffles the given array into a random order
+ * @param {Array} arr 
+ */
 const randomizeArray = (arr) => {
+  // Fischer-Yates modern
   for (let i = arr.length - 1; i >= 0; i--) {
     const r = Math.floor(Math.random() * i);
     const temp = arr[i];

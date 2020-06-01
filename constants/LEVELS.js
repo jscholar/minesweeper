cfeb79763af10e8339f6c160c1bc6ac2c6af936f
Jin Cai <jinruncai@gmail.com>
2020-06-01 11:35:48 -0700

    Separate levels constants into different file

---
+++ constants/LEVELS.js
@@ -0,0 +1,22 @@
+const LEVELS = {
+    1: {
+      name: 'easy',
+      width: 10,
+      height: 10,
+      mines: 10,
+    },
+    2: {
+      name: 'medium',
+      width: 20,
+      height: 20,
+      mines: 40,
+    },
+    3: {
+      name: 'hard',
+      width: 30,
+      height: 30,
+      mines: 100,
+    }
+  }
+
+module.exports = LEVELS;
--- controller/Game.js
+++ controller/Game.js
@@ -1,6 +1,8 @@
 const readline = require("readline");
 const Board = require('../models/Board');
 
+const LEVELS = require('../constants/LEVELS');
+
 class Game {
   board;
   playing = false;
@@ -11,41 +13,37 @@ class Game {
       output: process.stdout
     });
 
-    this.difficulties = {
-      1: {
-        width: 10,
-        height: 10,
-        mines: 10,
-      },
-      2: {
-        width: 20,
-        height: 20,
-        mines: 40,
-      },
-      3: {
-        width: 30,
-        height: 30,
-        mines: 100,
-      }
-    }
+
   }
 
   startGame() {
-    this.rl.question('Choose a difficulty [1: easy (default), 2: medium, 3: hard] (1,2,3): ', (diff = 1) => {
-      if (this.difficulties[diff] === undefined) {
-        this.startGame();
-      } else {
-        const { width, height, mines } = this.difficulties[diff];
-        this.board = new Board(width, height);
-        this.board.addMines(mines);
-        playing = true;
-        this.nextTurn();
-      }
-    });
+    const levelPrompt = `Choose a level [ ${ 
+      Object.entries(LEVELS)
+        .map(([level, { name }]) => (
+          `${level} (${name})`
+        ))
+      .join(', ')
+    } ]: `;
+    
+    this.rl.question(
+      levelPrompt,
+      (diff = 1) => {
+        if (LEVELS[diff] === undefined) {
+          this.startGame();
+        } else {
+          const { width, height, mines } = LEVELS[diff];
+          this.board = new Board(width, height);
+          this.board.addMines(mines);
+          playing = true;
+          this.nextTurn();
+        }
+      });
   }
 
   nextTurn() {
+
   }
+  
 }
 
 module.exports = Game;
